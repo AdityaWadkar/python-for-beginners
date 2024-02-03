d9c4350a226571e9f752cdc05b8b41bfc9c6c213
Aditya Wadkar <67093170+AdityaWadkar@users.noreply.github.com>
2024-02-03 19:29:44 +0530

    Update EP-20.py

--- EP-20.py
+++ EP-20.py
@@ -11,18 +11,18 @@ my_set = {1, 2, 3, 4, 5,5,5}
 print("Set:", my_set)
 
 # Adding elements to a set
-# my_set.add(6)
-# print("Set with added element:", my_set)
+my_set.add(6)
+print("Set with added element:", my_set)
 
 # # Removing elements from a set
-# my_set.remove(3)
-# print("Set with removed element:", my_set)
+my_set.remove(3)
+print("Set with removed element:", my_set)
 
 # Set operations (union, intersection)
-# set1 = {1, 2, 3, 4}
-# set2 = {3, 4, 5, 6}
-# print("Union:", set1.union(set2))
-# print("Intersection:", set1.intersection(set2))
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+print("Union:", set1.union(set2))
+print("Intersection:", set1.intersection(set2))
 
 #Heterogeneous property
 my_set = {1, 2, 'apple', 3.14, True}
