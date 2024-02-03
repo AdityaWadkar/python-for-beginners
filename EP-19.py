e1492f88f568aad78af9045cdf8d23845bc4cc45
Aditya Wadkar <67093170+AdityaWadkar@users.noreply.github.com>
2024-02-03 19:23:05 +0530

    Add files via upload

---
+++ EP-19.py
@@ -0,0 +1,32 @@
+# DAY-19: Dictionaries
+
+"""
+Mutable: add, remove, or modify key-value pairs.
+Unordered: not stored in any specific order
+Dynamic: grow and shrink after creation
+Heterogeneous: values can be of any data types
+"""
+# Dictionaries
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+print("Dictionary:", my_dict)
+
+# Accessing values in a dictionary
+# print("Age:", my_dict['age'])
+
+# # Modifying values in a dictionary
+# my_dict['age'] = 26
+# print("Modified dictionary:", my_dict)
+
+# # Adding a new key-value pair
+# my_dict['gender'] = 'Male'
+# print("Dictionary with new pair:", my_dict)
+
+# # adding multiple types of values
+my_dict = {'name': 'John', 
+           'age': 25, 
+           'is_student': False, 
+           'grades': [90, 85, 95], 
+           'city': 'New York'}
+
+print(my_dict)
+
---
+++ EP-20.py
@@ -0,0 +1,30 @@
+# DAY-20: Sets
+
+"""
+Mutable: add and remove elements after the set is created.
+Unordered: not stored in any specific order
+*Unique Elements: only contain unique elements
+Heterogeneous : Elements can be of any data type
+"""
+#creation
+my_set = {1, 2, 3, 4, 5,5,5}
+print("Set:", my_set)
+
+# Adding elements to a set
+# my_set.add(6)
+# print("Set with added element:", my_set)
+
+# # Removing elements from a set
+# my_set.remove(3)
+# print("Set with removed element:", my_set)
+
+# Set operations (union, intersection)
+# set1 = {1, 2, 3, 4}
+# set2 = {3, 4, 5, 6}
+# print("Union:", set1.union(set2))
+# print("Intersection:", set1.intersection(set2))
+
+#Heterogeneous property
+my_set = {1, 2, 'apple', 3.14, True}
+print(my_set)
+
