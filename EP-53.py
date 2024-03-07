3816bd38d3f840351edb7a2ecc7080be9be97a1f
Aditya Wadkar <67093170+AdityaWadkar@users.noreply.github.com>
2024-03-07 16:28:21 +0530

    Add files via upload

---
+++ EP-53.py
@@ -0,0 +1,30 @@
+# Day-53: Operator Overloading
+
+"""
+Operator overloading is a mechanism that 
+allows us to give new meaning to existing operators
+
+"""
+
+# Python program to show use of
+print(3+5)
+# + operator for different purposes.
+# concatenate two strings
+print("project"+"maker")
+
+# Product two numbers
+print(3*4)
+
+# Repeat the String
+print("project "*5)
+
+
+
+
+
+"""
+Task (project):-
+create end to project like :-
+library management system
+hotel management system
+"""
